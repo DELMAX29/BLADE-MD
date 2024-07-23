11b5003e785ddf6bd8c4a219c0a1941d49c1f7b6
DELMAX29 <154527751+DELMAX29@users.noreply.github.com>
2024-07-23 23:00:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || '2348059540212'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || '263717672068'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://mohsin:mohsin@cluster0.iauaztt.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'maxdavid126@gmail.com'
@@ -15,12 +15,12 @@ global.devs = '2348069553870'
 global.website = 'https://github.com/Bladeh4x/BLADE-MD' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3f3ae4aace2d012c11509.jpg'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
+  sessionName: process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMEVHY1Q4TDdoM0pHaHQrb1V6aWNsdVZDbnlta0hyVUFsVGlXcXBDZXFYMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZFVuN29UNHdEMlBjTzMzbDhDMmtUSitIZm9GN1oxT0ZlTHJ1S1VTNmNTTT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJRTVFudnAwbVB3WjJ1bVkrTjJ5T3VXeG05d1NDYkcwMWRwNTNXb1FrSlVnPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ4cTk3Qjg5V2RNaklPdERpVUVqdEdaMDJJNkJKeVFJaGxpblRlVmo5SkE0PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFLN2JZRnFGVDdqTGZUaENXTWZMWjJvT1ZDcmRLSE40UE5qN0lqRDhCWDA9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im96SlJCNVZwcW5IbDB5L3dVREJ6RzlQQXBCYW50Vi91RjlVUW1Ya1EveXM9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiU0FoekUwL1ovcnh5MG5Sb2hUUnd3YVVueC9Qbm9wblpnWTgvRmhOaE0ybz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVFdoOHp1QjBoTU9KN1BRR2w3UDRUQVp2WWQ5VVJPYW5KbklsWWg1OGQzbz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ims1UElGZWpzTjA5T2h5b0JtcWVrNWxON0FRejZablB3b1d6UnNjVGQ3R1g4RWhDek1ON2NDQ3VVYnQySzY0RXZLeHN0QmxkS0NoWHpoL0VrbDVEUmhRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NTUsImFkdlNlY3JldEtleSI6ImhhMjJJc3dLQ0t0M25yUXZUTitaZmhXTjhHNjM0eEErSW1IZ1pteGtkajA9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImVXSjlYUEd3UWRXMHdkZjUzMjd0cHciLCJwaG9uZUlkIjoiMDEyNzkxYjUtOGRlMi00YTlhLWI4YjctZjYyM2VkNTViZWM2IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im9OeGxXQ0RTRkFkSEFDY3owSXpQdWVGQ05rQT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ6M1QwaGtjWFhuT2h6clZPdXdIRGdwcjFGTXc9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiWllZODNWNTgiLCJtZSI6eyJpZCI6IjI2Mzc4NzM4Mjg1MjoyNUBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDSTJuMFpBREVLR3lnTFVHR0FjZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiMnVHUkVrNnVMZTFKRis4bUFNaFpUUERPU0phTTFoTnI2cWQ4L2hkKzl3RT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiV0F2czhpWUtRMm9vZlJheHg5MW01ZDV0QmNodGgxSDdNZUxIT3VQMEhETG5HYlV2bXNvVkhDaFpPZWpjTmQ0S0o3M1Z5UFlqMUJEU1BaOC8yMmtWQ3c9PSIsImRldmljZVNpZ25hdHVyZSI6ImlTSnJFY3Z5MTRvakQ5S2hsMStiOWVMeEFrVThUZzBqMXdtYVAvZGROSWNkc1gzclBCOWhuS1VxQWZBZlFkVU5oZmFVZVoybVg1REVaWlVZd1VMb2dnPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjYzNzg3MzgyODUyOjI1QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmRyaGtSSk9yaTN0U1JmdkpnRElXVXp3emtpV2pOWVRhK3FuZlA0WGZ2Y0IifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjE3NjgyMzksIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBRFA2In0=",      //Put Your Session Id Here
   author:  process.env.PACK_AUTHER ||  'BLADE BOT',
-  packname:  process.env.PACK_NAME || 'MADE BY ALEX DAVID',
+  packname:  process.env.PACK_NAME || 'MADE BY K29promax',
   
   botname:   process.env.BOT_NAME === undefined ? "BLADE-MD" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'blade md' : process.env.OWNER_NAME,  
+  ownername: process.env.OWNER_NAME === undefined ? 'k29promax : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
@@ -28,7 +28,7 @@ module.exports = {
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
-  HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
+  HANDLERS:  process.env.PREFIX === undefined ? ',.' : process.env.PREFIX,
   warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
   disablepm:  process.env.DISABLE_PM === undefined ? true : process.env.DISABLE_PM,
   levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
